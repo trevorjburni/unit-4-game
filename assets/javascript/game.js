44f2e7ebf74135f1b94c4f40226d51821718e751
Scoop <trevorjburni@gmail.com>
2019-11-04 19:32:40 -0700

    Created most of the HTML and created a few variables I'll use in the game.js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Crystal Collector Game
+
+// Need to generate 5 random numbers
+// The first random number should be between 19-120. This number is the number the user is trying to get to.
+// The other four random numbers should be between 1-12.
+
+var goal = 0;
+var diamondValue = 0;
+var emeraldValue = 0;
+var citrineValue = 0;
+var garnetValue = 0;
