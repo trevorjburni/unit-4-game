bf32e7d624bcd8a608e272ca6df1611ddd106670
Scoop <trevorjburni@gmail.com>
2019-11-09 16:58:55 -0700

    Properly linked the javascript file to HTML, added the game functionality to the javascript file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,50 +5,91 @@
 // The other four random numbers should be between 1-12.
 
 // Declare our variables goal, crystal, wins, losses and currentTotal.
-$(document).ready(function() {
-var goal = 0;
-var crystalOne = 0;
-var crystalTwo = 0;
-var crystalThree = 0;
-var crystalFour = 0;
+$(document).ready(function () {
+    var goal = 0;
+    var crystalOne = 0;
+    var crystalTwo = 0;
+    var crystalThree = 0;
+    var crystalFour = 0;
 
-var wins = 0;
-var losses = 0;
+    var wins = 0;
+    var losses = 0;
 
-var currentTotal = 0;
-
-var isInProgress = false;
+    var currentTotal = 0;
 
 
-// Write a start function
-function startGame() {
-    reset();
-    randomGoalNumber();
-    randomCrystalNumber(crystalOne);
-    randomCrystalNumber(crystalTwo);
-    randomCrystalNumber(crystalThree);
-    randomCrystalNumber(crystalFour);
-    console.log()
-}
+    // Write a start function
+    function startGame() {
+        reset();
+        randomGoalNumber();
+        randomCrystalNumber();
+    }
 
-// Write a function that resets the values of each goal, each crystal and current total.
-function reset() {
-    goal = 0;
-    crystalOne = 0;
-    crystalTwo = 0;
-    crystalThree = 0;
-    crystalFour = 0;
-}
+    // Write a function that resets the values of each goal, each crystal and current total.
+    function reset() {
+        goal = 0;
+        crystalOne = 0;
+        crystalTwo = 0;
+        crystalThree = 0;
+        crystalFour = 0;
+        currentTotal = 0;
 
-// write a function that generates a random goal number.
-function randomGoalNumber() {
-    goal = Math.floor(Math.random() * 102) + 19;
-}
+        $("#total-score").html(currentTotal);
+    };
 
-// Write a function that generates a random crystaln number.
-function randomCrystalNumber(crystal) {
-    crystal = Math.floor(Math.random() * 12) + 1;
-}
+    // write a function that generates a random goal number.
+    function randomGoalNumber() {
+        goal = Math.floor(Math.random() * 102) + 19;
+        $("#goal").html(goal);
+    };
 
-startGame();
+    // Write a function that generates a random crystaln number.
+    function randomCrystalNumber() {
+        crystalOne = Math.floor(Math.random() * 12) + 1;
+        crystalTwo = Math.floor(Math.random() * 12) + 1;
+        crystalThree = Math.floor(Math.random() * 12) + 1;
+        crystalFour = Math.floor(Math.random() * 12) + 1;
+    };
+
+    // Write a function that checks to see if the user has won or lost.
+    function check() {
+        if (currentTotal === goal) {
+            wins++;
+            $("#wins").html(wins);
+            startGame();
+        } else if (currentTotal > goal) {
+            losses++;
+            $("#losses").html(losses);
+            startGame();
+        } else {
+            return;
+        }
+    }
+
+    $(".crystal-1").on("click", function () {
+        currentTotal += crystalOne;
+        $("#total-score").html(currentTotal);
+        check();
+
+    });
+    $(".crystal-2").on("click", function () {
+        currentTotal += crystalTwo;
+        $("#total-score").html(currentTotal);
+        check();
+
+    });
+    $(".crystal-3").on("click", function () {
+        currentTotal += crystalThree;
+        $("#total-score").html(currentTotal);
+        check();
+
+    });
+    $(".crystal-4").on("click", function () {
+        currentTotal += crystalFour;
+        $("#total-score").html(currentTotal);
+        check();
+
+    });
+    startGame();
+
 });
